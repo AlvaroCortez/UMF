9d6996d5fda5d36ad3b3c89d10653bfb2a237aa1
Pavel Fadeev <malachornix@gmail.com>
2015-11-14 23:27:25 +0400

    Оценка остатка ряда
    Добавил тестовый класс для оценки остатка ряда.

---
+++ UMF/src/course/Test.java
@@ -0,0 +1,35 @@
+public class Test {
+
+	public static double row(double n) {
+         return 1/Math.pow(n, 3);
+     }
+
+    public static double s(double l, double k) {
+//        return 4*l*l/(k*Math.pow(Math.PI, 3));
+        return 4*0.04*l*l/Math.PI;
+//        return 4/Math.PI;
+    }
+
+    public static int findN(double eps) {
+        int N = 1;
+        double origin = s(1.0, 0.005) * 1.20205690315031;
+        double result = s(1.0, 0.005) * row(N + 1);
+        if (Math.abs(origin - result) >= eps) {
+            return N;
+        } else {
+            N++;
+            N++;
+            while (true) {
+                result += s(1.0, 0.005) * Math.abs(s(1, 0.005) * row(N));
+                if (Math.abs(origin - result) >= eps) {
+                    return N;
+                }
+                N++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findN(0.1));
+    }
+}
