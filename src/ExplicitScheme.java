28990e9d1aac41c03cb9de1bc1afd76f8c0d2711
Pavel Fadeev <malachornix@gmail.com>
2016-03-31 10:52:06 +0400

    Баг фикс.

--- src/ExplicitScheme.java
+++ src/ExplicitScheme.java
@@ -25,14 +25,18 @@ public class ExplicitScheme {
     private double[][] gridValues;
 
     public double[][] scheme(double hx, double ht) {
-        gridValues = new double[(int) (T / ht + 1)][(int) (ht / hx) + 1];
+        gridValues = new double[(int) (T / ht + 1)][(int) (L / hx) + 1];
         double I = gridValues[0].length;
         double K = gridValues.length;
 
         // TODO: 28.03.2016 Возможно жопа с границами
-        for (int i = 0; i < I; i++) {
+        for (int i = 0; i < I - 1 /*i < I*/; i++) {
             gridValues[0][i] = PSI(hx * i) - U0;
         }
+
+        // Сделал проверку, чтобы убедиться, что на границе возможно должен быть ноль.
+        // System.out.println(gridValues[0][gridValues[0].length - 1]);
+
         for (int k = 1; k < K - 1; k++) {
             for (int i = 1; i < I - 1; i++) {
                 gridValues[k][i] = uikplus1(gridValues[k - 1][i + 1], gridValues[k - 1][i], gridValues[k - 1][i - 1], hx, ht, PHI(hx * i));
@@ -43,8 +47,14 @@ public class ExplicitScheme {
     }
 
     private double PHI(double xi) {
+        if (xi >= L / 3 && xi <= 2 * L / 3) {
+            return 1;
+        } else {
+            return 0;
+        }
 
-        return (Math.sin(2 * Math.PI * xi / 3) - Math.sin(Math.PI * xi / 3)) * 4 / (2 * Math.PI * xi + Math.sin(2 * Math.PI * xi));
+
+        // return (Math.sin(2 * Math.PI * xi / 3) - Math.sin(Math.PI * xi / 3)) * 4 / (2 * Math.PI * xi + Math.sin(2 * Math.PI * xi));
     }
 
     private double PSI(double x){
@@ -62,22 +72,28 @@ public class ExplicitScheme {
         XYSeriesCollection xySeriesCollection = new XYSeriesCollection();
         XYSeries xySeries = new XYSeries("Явная схема");
         ExplicitScheme explicitScheme = new ExplicitScheme();
-        double hx = 50;
-        double ht = 300;
-        double[][] scheme = explicitScheme.scheme(0.001, 0.02);
+        double ht = 0.02;
+        double hx = 0.001;
+        double[][] scheme = explicitScheme.scheme(hx, ht);
         double x = 0;
+        // int level = 1630;
+        // double temperature = level * ht;
+        double temperature = 32.6;
+        int level = (int) (temperature / ht);
         for (int i = 0; i < scheme[0].length; i++) {
-            xySeries.add(x, scheme[5][i]);
-            x += hx;
+            xySeries.add(x, scheme[level][i]);
+            x += 0.001;
         }
         xySeriesCollection.addSeries(xySeries);
-        JFreeChart pl = ChartFactory.createXYLineChart("", "x, кол-во слагаемых N = ", "u(x), при t = " ,
+        JFreeChart pl = ChartFactory.createXYLineChart("", "x, кол-во слагаемых N = ", "u(x), при t = " + temperature,
                 xySeriesCollection, PlotOrientation.VERTICAL, true, true, false);
         JFrame jFrame = new JFrame();
         ChartPanel chartPanel = new ChartPanel(pl);
         jFrame.add(chartPanel);
         chartPanel.setVisible(true);
         jFrame.setVisible(true);
+        jFrame.setSize(800, 600);
+        jFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
 
     }
