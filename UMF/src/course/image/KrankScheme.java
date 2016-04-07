bbea18dd3c72d3ac98672bbf689265795cd291e1
Pavel Fadeev <malachornix@gmail.com>
2016-04-07 10:24:43 +0400

    Безуспешная попытка сделать метод прогонки. :( Все изменения закомментированы на всякий случай.

--- UMF/src/course/image/KrankScheme.java
+++ UMF/src/course/image/KrankScheme.java
@@ -48,20 +48,35 @@ public class KrankScheme extends Scheme{
         for (int k = 1; k <= K; k++) {
             alpha[0] = 0.1;
             beta[0] = 0.1;
+            // b[0] = 1 + G + gamma;
+            // f[0] = gridValues[0][1] * gamma + ht / C * (hx);
+            // c[0] = -gamma / 2;
+            // alpha[0] = c[0] / b[0];
+            // beta[0] = f[0] / b[0];
+
             for (int i = 1; i < I; i++) {
                 a[i] = -gamma/2;
+                // b[i] = 1 + G + gamma;
                 b[i] = 1 - G + gamma;
                 c[i] = -gamma/2;
+                // f[i] = gridValues[k-1][i-1] *(-a[i]) + gridValues[k-1][i] * (1 - G + gamma) + gridValues[k-1][i+1] * (-c[i]) + (ht/C)*PHI(hx * i);
                 f[i] = gridValues[k-1][i-1] *a[i] + gridValues[k-1][i] * b[i] + gridValues[k-1][i+1] * c[i] + (ht/C)*PHI(hx * i);
+
+                // alpha[i] = c[i] / (b[i] - a[i] * alpha[i - 1]);
                 alpha[i] = - a[i] / (b[i] + c[i] * alpha[i - 1]);
+                // beta[i] = (f[i] - a[i] * beta[i - 1]) / (b[i] - a[i] * alpha[i - 1]);
                 beta[i] = (f[i] - c[i] * beta[i - 1]) / (b[i] + c[i] * alpha[i - 1]);
             }
 
             a[I] = -gamma/2;
+            // b[I] = 1 + G + gamma;
             b[I] = 1 - G + gamma;
             c[I] = -gamma/2;
+            // f[I] = gridValues[k-1][I-1] *(-a[I]) + gridValues[k-1][I] * (1 - G + gamma) + gridValues[k-1][I-1] * (-c[I]) + (ht/C)*PHI(hx * I);
             f[I] = gridValues[k-1][I-1] *a[I] + gridValues[k-1][I] * b[I] + gridValues[k-1][I-1] * c[I] + (ht/C)*PHI(hx * I);
+            // alpha[I] = c[I] / (b[I] - a[I] * alpha[I - 1]);
             alpha[I] = - a[I] / (b[I] + c[I] * alpha[I - 1]);
+            // beta[I] = (f[I] - a[I] * beta[I - 1]) / (b[I] + a[I] * alpha[I - 1]);
             beta[I] = (f[I] - c[I] * beta[I - 1]) / (b[I] + c[I] * alpha[I - 1]);
 
             gridValues[k][I] = (gridValues[k - 1][I] - a[I]* beta[I - 1] + c[I]* beta[I - 1])/(a[I]* alpha[I - 1] + b[I] + c[I]* alpha[I - 1]);
