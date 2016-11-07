62c5675e3aad7cd1c94dede8f1145e4d8f896eb0
anka_47 <i_in_mail@mail.ru>
2016-11-07 22:33:27 +0400

    fix few bugs

---
+++ UMF/src/course/TestClass.java
@@ -0,0 +1,10 @@
+package course;
+
+/**
+ * Created by anka_47 on 07.11.16.
+ */
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
